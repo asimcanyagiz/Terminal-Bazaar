00ae22282045e386c8238a9a0bf5fbc8a0a1a48e
Asim Can Yagiz <meycasim@gmail.com>
2022-09-25 20:07:53 +0300

    Boss subclass for boss fight

---
+++ iOS-Bootcamp-Week1/Model/Enemies.swift
@@ -0,0 +1,95 @@
+//
+//  Enemies.swift
+//  iOS-Bootcamp-Week1
+//
+//  Created by Asım can Yağız on 25.09.2022.
+//
+
+import Foundation
+
+// Kraken the Boss of Sub class of User
+class BossKraken: User {
+    var bossImage = """
+             _                      _______                      _
+          _dMMMb._              .adOOOOOOOOOba.              _,dMMMb_
+         dP'  ~YMMb            dOOOOOOOOOOOOOOOb            aMMP~  `Yb
+         V      ~"Mb          dOOOOOOOOOOOOOOOOOb          dM"~      V
+                  `Mb.       dOOOOOOOOOOOOOOOOOOOb       ,dM'
+                   `YMb._   |OOOOOOOOOOOOOOOOOOOOO|   _,dMP'
+              __     `YMMM| OP'~"YOOOOOOOOOOOP"~`YO |MMMP'     __
+            ,dMMMb.     ~~' OO     `YOOOOOP'     OO `~~     ,dMMMb.
+         _,dP~  `YMba_      OOb      `OOO'      dOO      _aMMP'  ~Yb._
+
+                     `YMMMM`OOOo     OOO     oOOO'/MMMMP'
+             ,aa.     `~YMMb `OOOb._,dOOOb._,dOOO'dMMP~'       ,aa.
+           ,dMYYMba._         `OOOOOOOOOOOOOOOOO'          _,adMYYMb.
+          ,MP'   `YMMba._      OOOOOOOOOOOOOOOOO       _,adMMP'   `YM.
+          MP'        ~YMMMba._ YOOOOPVVVVVYOOOOP  _,adMMMMP~       `YM
+          YMb           ~YMMMM`OOOOI`````IOOOOO'/MMMMP~           dMP
+           `Mb.           `YMMMb`OOOI,,,,,IOOOO'dMMMP'           ,dM'
+             `'                  `OObNNNNNdOO'                   `'
+                                   `~OOOOO~'
+
+                                 BOSS KRAKEN
+                    
+                              1:ATTACK 2:ESCAPE
+"""
+    var bossImage1 = """
+             _                      _______
+          _dMMMb._              .adOOOOOOOOOba.
+         dP'  ~YMMb            dOOOOOOOOOOOOOOOb
+         V      ~"Mb          dOOOOOOOOOOOOOOOOOb
+                  `Mb.       dOOOOOOOOOOOOOOOOOOOb
+                   `YMb._   |OOOOOOOOOOOOOOOOOOOOO|
+              __     `YMMM| OP'~"YOOOOOOOOOOOP"~`YO
+            ,dMMMb.     ~~' OO     `YOOOOOP'     OO
+         _,dP~  `YMba_      OOb      `OOO'      dOO
+
+                     `YMMMM`OOOo     OOO     oOOO'/MMMMP'
+             ,aa.     `~YMMb `OOOb._,dOOOb._,dOOO'dMMP~'       ,aa.
+                             `OOOOOOOOOOOOOOOOO'          _,adMYYMb.
+                               OOOOOOOOOOOOOOOOO       _,adMMP'   `YM.
+                               YOOOOPVVVVVYOOOOP  _,adMMMMP~       `YM
+                              `OOOOI`````IOOOOO'/MMMMP~           dMP
+                               `OOOI,,,,,IOOOO'dMMMP'           ,dM'
+                                 `OObNNNNNdOO'                   `'
+                                   `~OOOOO~'
+
+                                 BOSS KRAKEN
+                    
+                              1:ATTACK 2:ESCAPE
+"""
+    var bossImage2 = """
+
+                                .adOOOOOOOOOba.
+                               dOOOOOOOOOOOOOOOb
+                              dOOOOOOOOOOOOOOOOOb
+                            dOOOOOOOOOOOOOOOOOOOb
+                            |OOOOOOOOOOOOOOOOOOOOO|
+                            OP'~"YOOOOOOOOOOOP"~`YO
+                            OO     `YOOOOOP'     OO
+                            OOb      `OOO'      dOO
+                            OOOo     OOO     oOOO'/
+                            `OOOb._,dOOOb._,dOOO'
+                             `OOOOOOOOOOOOOOOOO'
+                               OOOOOOOOOOOOOOOOO
+                               YOOOOPVVVVVYOOOOP
+                                OOOOI`````IOOOOO'
+                                `OOOI,,,,,IOOOO'
+                                 `OObNNNNNdOO'
+                                   `~OOOOO~'
+
+                                 BOSS KRAKEN
+                    
+                              1:ATTACK 2:ESCAPE
+"""
+    var bossImage3 = """
+
+                                    DEAD
+
+                                 BOSS KRAKEN
+"""
+    
+  
+}
+
